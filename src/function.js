7047d0f8668002d2ed354df535fd56e8bbbb96a5
ESSADKI123 <mohamad.essadki11@email.com>
2026-09-17 18:43:31 +0100

    projet-final-sas

---
+++ src/function.js
@@ -0,0 +1,33 @@
+import {apprenants} from './data.js';
+
+export function normalisernom(nom) {
+    return nom.trim().toLowerCase();
+}
+export function validerResultat(jour, exercicesTermines, totalExercices,) {
+    if (jour < 1 || jour > 7) {
+        return false;
+    }
+
+    if (exercicesTermines > totalExercices) {
+        return false;
+    }
+
+    if ( exercicesTermines < 0 || totalExercices < 0){
+        return false; 
+    }
+    return true;
+}
+export function ajouterApprenant ( nomComplet, ville){
+ for ( let i of apprenants)
+    if (nomComplet === i.nomComplet)
+        return false ; 
+
+let newStudent = {
+    id : apprenants.length + 1, 
+    nomcomplet : nomComplet,
+    ville : ville,
+    resultats : []
+}
+apprenants.push(newStudent)
+        return true 
+}
---
+++ src/index.js
@@ -0,0 +1,3 @@
+import { validerResultat } from "./function.js";
+
+console.log(validerResultat(1,17,10));
