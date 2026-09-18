07ee3232585878a8523d183d5c5b4eb06c86e263
ESSADKI123 <mohamad.essadki11@email.com>
2026-09-18 17:19:14 +0100

    tada.js edit

--- src/data.js
+++ src/data.js
@@ -1,22 +1,85 @@
 export const apprenants = [
-{
-id: 1,
-nomComplet: "Sara Dev",
-ville: "Nador",
-resultats: [
-{ jour: 1, exercicesTermines: 18,
-totalExercices: 20, challengeTermine: true },
-{ jour: 2, exercicesTermines: 14,
-totalExercices: 20, challengeTermine: false }
-]
-},
-{
-id: 2,
-nomComplet: "Yassine Code",
-ville: "Oujda",
-resultats: [
-{ jour: 1, exercicesTermines: 12,
-totalExercices: 20, challengeTermine: false }
-]
-}
+  {
+    id: 1,
+    nomComplet: "Sara Dev",
+    ville: "Nador",
+    resultats: [
+      { jour: 1, exercicesTermines: 18, totalExercices: 20, challengeTermine: true },
+      { jour: 2, exercicesTermines: 14, totalExercices: 20, challengeTermine: false }
+    ]
+  },
+  {
+    id: 2,
+    nomComplet: "Yassine Code",
+    ville: "Oujda",
+    resultats: [
+      { jour: 1, exercicesTermines: 12, totalExercices: 20, challengeTermine: false }
+    ]
+  },
+  {
+    id: 3,
+    nomComplet: "ahmed",
+    ville: "Oujda",
+    resultats: [
+      { jour: 1, exercicesTermines: 17, totalExercices: 20, challengeTermine: false }
+    ] 
+  },
+  {
+    id: 4,
+    nomComplet: "mouad",
+    ville: "nador",
+    resultats: [
+      { jour: 1, exercicesTermines: 17, totalExercices: 20, challengeTermine: false },
+      { jour: 2, exercicesTermines: 18, totalExercices: 20, challengeTermine: true },
+      { jour: 3, exercicesTermines: 14, totalExercices: 20, challengeTermine: false }
+    ]
+  },
+  {
+    id: 5,
+    nomComplet: "youssef",
+    ville: "nador",
+    resultats: [
+      { jour: 1, exercicesTermines: 16, totalExercices: 20, challengeTermine: false }
+    ] 
+  },
+  {
+    id: 6,
+    nomComplet: "mohamed",
+    ville: "berkane",
+    resultats: [
+      { jour: 1, exercicesTermines: 19, totalExercices: 20, challengeTermine: false },
+      { jour: 2, exercicesTermines: 18, totalExercices: 20, challengeTermine: true },
+    ] 
+  },
+  {
+    id: 7,
+    nomComplet: "mohamed",
+    ville: "ourzazat",
+    resultats: [
+      { jour: 1, exercicesTermines: 20, totalExercices: 20, challengeTermine: false },
+    ] 
+  },
+  {
+    id: 8,
+    nomComplet: "azez",
+    ville: "berkae",
+    resultats: [
+      { jour: 1, exercicesTermines: 10, totalExercices: 20, challengeTermine: true },
+      { jour: 2, exercicesTermines: 10, totalExercices: 20, challengeTermine: true },
+      { jour: 3, exercicesTermines: 14, totalExercices: 20, challengeTermine: true }
+    ]
+  },
+  {
+    id: 9,
+    nomComplet: "moulod",
+    ville: "berkae",
+    resultats: [
+      { jour: 1, exercicesTermines: 10, totalExercices: 20, challengeTermine: true },
+      { jour: 2, exercicesTermines: 10, totalExercices: 20, challengeTermine: true },
+      { jour: 3, exercicesTermines: 14, totalExercices: 20, challengeTermine: true },
+      { jour: 4, exercicesTermines: 17, totalExercices: 20, challengeTermine: true },
+      { jour: 5, exercicesTermines: 15, totalExercices: 20, challengeTermine: true },
+      { jour: 6, exercicesTermines: 14, totalExercices: 20, challengeTermine: true }
+    ]
+  }
 ];
